b8319e94dbeb695bb668ff0da4342c82f1ff4b57
Maria Julia Lobo <65983895+mjulialobo@users.noreply.github.com>
2021-10-24 02:15:31 -0300

    Delete projetos/algoritimo-unit-test directory

--- projetos/algoritimo-unit-test/unit-tests/ex-7.test.js
+++
@@ -1,9 +0,0 @@
-const { expect, test } = require('@jest/globals');
-const { carCost } = require('../exercises/ex-7');
-
-test('shouldReturnCarCost', () => {
-	expect(carCost(15000)).toBe(64590);
-});
-test('shouldReturnError', () => {
-	expect(carCost('test')).toBe('Por favor, insira somente números.');
-});
