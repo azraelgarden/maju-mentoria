27fd4b746f43cecd765beb88ee9e2d3d92762996
Maria Julia Lobo <mjulialobo23@gmail.com>
2022-02-10 13:41:19 -0300

    feat: home page and components

--- projetos/ts-exercises/exercises/ex-1.js
+++ projetos/ts-exercises/exercises/ex-1.js
@@ -1,14 +1,18 @@
-//Elaborar um algoritmo que lê 2 valores a e b e os escreve com a mensagem:
+"use strict";
+exports.__esModule = true;
+exports.isMultiple = void 0;
+//Elaborar um algoritmo que lê 2 valores a e b e os escreve com a mensagem: 
 //"São múltiplos" ou "Não são múltiplos".
 function isMultiple(a, b) {
-	var division = a % b;
-	if (division === 0) {
-		console.log('São múltiplos');
-		return 'multiplos';
-	} else {
-		console.log('Não são múltiplos');
-		return 'não-multiplos';
-	}
+    var division = a % b;
+    if (division === 0) {
+        console.log("São múltiplos");
+        return 'multiplos';
+    }
+    else {
+        console.log("Não são múltiplos");
+        return 'não-multiplos';
+    }
 }
-isMultiple(20, 5);
-module.exports = { isMultiple: isMultiple };
+exports.isMultiple = isMultiple;
+isMultiple(16, 5);
